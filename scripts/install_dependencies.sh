de598b66072ed3dc6a4ffc0a260689cc479938a3
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-15 16:48:41 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -30,7 +30,7 @@ sudo docker network rm -f aura-network || true
 
 docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-hrms-db:latest
 
-docker network create aura-network
+# docker network create aura-network
 
 
 # docker run -d --name aura-app --hostname localhost -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 80:80 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/static-website:latest
