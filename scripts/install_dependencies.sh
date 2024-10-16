9ee43512ace79d9b7e5c3e8d911a02ec2d8e5cc9
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-16 13:28:51 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -28,10 +28,19 @@ sudo docker rm -f aura-db || true
 
 #sudo docker network rm -f aura-network || true
 
-docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-hrms-db:latest
+docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:latest
 
-# docker network create aura-network
+docker network create aura-network
 
 
 # docker run -d --name aura-app --hostname localhost -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 80:80 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/static-website:latest
-docker run -d --name aura-db --hostname localhost -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 5432:5432 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-hrms-db:latest
+#docker run -d --name aura-db --hostname localhost -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 5432:5432 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-hrms-db:latest
+
+docker run -d \
+  --name aura-db \
+  --network aura-network \
+  --env POSTGRES_USER=postgres \
+  --env POSTGRES_PASSWORD=password \
+  --env POSTGRES_DB=hrms \
+  --publish published=5432,target=5432 \
+  270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:latest
