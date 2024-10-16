c94cafe1cd400ad091468887deda9ba72839f9fe
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-16 16:44:39 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -10,7 +10,9 @@ sudo apt-get install unzip
 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 #unzip awscliv2.zip
 #sudo ./aws/install
- 
+
+sudo apt install docker.io
+
 # Enable Docker service to start on boot
 sudo systemctl enable docker
 sudo systemctl start docker
