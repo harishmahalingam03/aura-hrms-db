2498e4124664cc9b6dc97bf67f91a5f85b992e66
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-18 10:25:53 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,14 +3,22 @@ set -e
 
 # Update package index and install docker.io
 sudo apt-get update -y
-sudo apt-get install unzip
 
-# To install the AWS CLI, following commands added
+yes | sudo apt-get install -y ca-certificates curl
 
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#sudo ./aws/install
+yes | sudo install -m 0755 -d /etc/apt/keyrings
 
+yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
+
+yes | echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+yes | sudo apt-get update -y
+
+yes | sudo apt-get install -y docker-ce docker-ce-cli docker-buildx-plugin docker-compose-plugin
+
+
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # Log in to AWS ECR
 TOKEN=$(aws ecr get-login-password --region us-east-1)
@@ -24,13 +32,20 @@ sudo rm -rf /home/ubuntu/aura-db || true
 
 sudo docker rm -f aura-db || true 
 
-sudo docker network rm aura-network || true
-
 #sudo docker network rm -f aura-network || true
 
 docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:latest
 
-docker network create aura-network
+NETWORK_NAME="aura-network"
+
+# Check if the network already exists
+if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
+    echo "Network '$NETWORK_NAME' already exists."
+else
+    # Create the network if it doesn't exist
+    docker network create "$NETWORK_NAME"
+    echo "Network '$NETWORK_NAME' created."
+fi
 
 
 # docker run -d --name aura-app --hostname localhost -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 80:80 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/static-website:latest
